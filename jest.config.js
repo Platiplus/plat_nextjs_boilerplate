2a6bacda7fb38a87c1baa8be32b923a3159b5010
Platiplus <quazarweb@gmail.com>
2021-02-18 22:32:16 -0300

    Excluding files from code coverage and serving static content on storybook

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,15 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.tsx',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx',
+    '!src/pages/index.tsx',
+    '!src/styles/**',
+    '!src/**/*.style.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
