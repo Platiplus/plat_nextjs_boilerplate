b6c726632bc2cc56b5edcd7985e184173172ffa4
Platiplus <quazarweb@gmail.com>
2021-02-19 09:50:12 -0300

    Better interface and modularity in generators

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -3,32 +3,71 @@ module.exports = function (plop) {
     description: 'Application Component Logic',
     prompts: [
       {
+        type: 'list',
+        name: 'type',
+        message: 'Qual tipo de arquivo você precisa?',
+        choices: ['Componente completo', 'Teste', 'Story', 'Estilo'],
+        filter: (val) => {
+          return val.toLowerCase()
+        }
+      },
+      {
         type: 'input',
         name: 'name',
-        message: 'Por favor digite o nome do componente:'
+        message: 'Por favor digite o nome do componente base:'
       }
     ],
-    actions: [
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
-        templateFile: 'templates/index.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
-        templateFile: 'templates/test.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.style.ts',
-        templateFile: 'templates/style.ts.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
+    actions: (data) => {
+      const actions = []
+      switch (data.type) {
+        case 'test':
+          actions.push({
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+            templateFile: 'templates/test.tsx.hbs'
+          })
+          break
+        case 'story':
+          actions.push({
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+            templateFile: 'templates/stories.tsx.hbs'
+          })
+          break
+        case 'style':
+          actions.push({
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.style.ts',
+            templateFile: 'templates/style.ts.hbs'
+          })
+          break
+
+        default:
+          actions.push(
+            {
+              type: 'add',
+              path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+              templateFile: 'templates/index.tsx.hbs'
+            },
+            {
+              type: 'add',
+              path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+              templateFile: 'templates/test.tsx.hbs'
+            },
+            {
+              type: 'add',
+              path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.style.ts',
+              templateFile: 'templates/style.ts.hbs'
+            },
+            {
+              type: 'add',
+              path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+              templateFile: 'templates/stories.tsx.hbs'
+            }
+          )
+          break
       }
-    ]
+      return actions
+    }
   })
 }
